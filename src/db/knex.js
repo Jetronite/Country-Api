c744776c1157ff9878d71a6693f0fe0b412a52f7
Jetronite <awajethro@gmail.com>
2025-10-30 13:00:24 +0100

    Fix knex config for Railway

--- src/db/knex.js
+++ src/db/knex.js
@@ -1,8 +1,10 @@
 import knex from "knex";
-import config from "../../knexfile.js";
+import configModule from "../../knexfile.js";
 
+const config = configModule.default || configModule; // support both CJS & ESM
 const environment = process.env.NODE_ENV || "development";
 const dbConfig = config[environment] || config.development;
+
 const db = knex(dbConfig);
 
 export default db;
