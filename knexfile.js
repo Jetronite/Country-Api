1896d51ac58c19a8e99a59209fd94eeadb616f96
Jetronite <awajethro@gmail.com>
2025-10-30 02:01:53 +0100

    fix: add production configuration to knexfile for database connection

--- knexfile.js
+++ knexfile.js
@@ -19,6 +19,17 @@ export default {
     },
     pool: { min: 0, max: 7 },
   },
+
+  production: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
+    migrations: { directory: "./migrations", extension: "js" },
+  },
 };
 
 
